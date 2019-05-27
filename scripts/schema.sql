5a233458de60047c7e57f6e07ce714b760202bec
Bo Henriksen <bomarconi@gmail.com>
2019-05-27 10:30:06 +0200

    Update schema.sql

--- scripts/schema.sql
+++ scripts/schema.sql
@@ -1,11 +1,13 @@
-USE master
-GO
-
-CREATE DATABASE c3NextGen
-
-GO
-
-USE c3NextGen
+info: Microsoft.EntityFrameworkCore.Infrastructure[10403]
+      Entity Framework Core 2.2.3-servicing-35854 initialized 'RepositoryContext' using provider 'Microsoft.EntityFrameworkCore.SqlServer' with options: None
+IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
 
 GO
 
@@ -53,18 +55,6 @@ CREATE TABLE [Component] (
 
 GO
 
-CREATE TABLE [FormFieldValue] (
-    [Id] bigint NOT NULL IDENTITY,
-    [Value] nvarchar(25) NULL,
-    [FormFieldId] bigint NOT NULL,
-    [CompletedFormId] bigint NOT NULL,
-    CONSTRAINT [PK_FormFieldValue] PRIMARY KEY ([Id]),
-    CONSTRAINT [FK_FormFieldValue_CompletedForm_CompletedFormId] FOREIGN KEY ([CompletedFormId]) REFERENCES [CompletedForm] ([Id]) ON DELETE CASCADE,
-    CONSTRAINT [FK_FormFieldValue_FormField_FormFieldId] FOREIGN KEY ([FormFieldId]) REFERENCES [FormField] ([Id]) ON DELETE NO ACTION
-);
-
-GO
-
 IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Name') AND [object_id] = OBJECT_ID(N'[Form]'))
     SET IDENTITY_INSERT [Form] ON;
 INSERT INTO [Form] ([Id], [Name])
@@ -83,13 +73,13 @@ GO
 IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'CompletedDate', N'FormId', N'UserId') AND [object_id] = OBJECT_ID(N'[CompletedForm]'))
     SET IDENTITY_INSERT [CompletedForm] ON;
 INSERT INTO [CompletedForm] ([Id], [CompletedDate], [FormId], [UserId])
-VALUES (CAST(1 AS bigint), '2019-05-16T00:00:00.0000000+02:00', CAST(1 AS bigint), CAST(1 AS bigint)),
-(CAST(2 AS bigint), '2019-05-16T00:00:00.0000000+02:00', CAST(2 AS bigint), CAST(2 AS bigint)),
-(CAST(3 AS bigint), '2019-05-16T00:00:00.0000000+02:00', CAST(3 AS bigint), CAST(3 AS bigint)),
-(CAST(4 AS bigint), '2019-05-16T00:00:00.0000000+02:00', CAST(4 AS bigint), CAST(1 AS bigint)),
-(CAST(5 AS bigint), '2019-05-16T00:00:00.0000000+02:00', CAST(5 AS bigint), CAST(2 AS bigint)),
-(CAST(6 AS bigint), '2019-05-16T00:00:00.0000000+02:00', CAST(6 AS bigint), CAST(1 AS bigint)),
-(CAST(7 AS bigint), '2019-05-16T00:00:00.0000000+02:00', CAST(7 AS bigint), CAST(1 AS bigint));
+VALUES (CAST(1 AS bigint), '2019-05-27T00:00:00.0000000+02:00', CAST(1 AS bigint), CAST(1 AS bigint)),
+(CAST(2 AS bigint), '2019-05-27T00:00:00.0000000+02:00', CAST(2 AS bigint), CAST(2 AS bigint)),
+(CAST(3 AS bigint), '2019-05-27T00:00:00.0000000+02:00', CAST(3 AS bigint), CAST(3 AS bigint)),
+(CAST(4 AS bigint), '2019-05-27T00:00:00.0000000+02:00', CAST(4 AS bigint), CAST(1 AS bigint)),
+(CAST(5 AS bigint), '2019-05-27T00:00:00.0000000+02:00', CAST(5 AS bigint), CAST(2 AS bigint)),
+(CAST(6 AS bigint), '2019-05-27T00:00:00.0000000+02:00', CAST(6 AS bigint), CAST(1 AS bigint)),
+(CAST(7 AS bigint), '2019-05-27T00:00:00.0000000+02:00', CAST(7 AS bigint), CAST(1 AS bigint));
 IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'CompletedDate', N'FormId', N'UserId') AND [object_id] = OBJECT_ID(N'[CompletedForm]'))
     SET IDENTITY_INSERT [CompletedForm] OFF;
 
@@ -100,9 +90,9 @@ IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Colu
 INSERT INTO [FormField] ([Id], [Column], [FormId], [Headline], [Height], [Row], [Static], [Width])
 VALUES (CAST(1 AS bigint), 3, CAST(1 AS bigint), N'Indtast Dit Navn', 3, 3, 0, 3),
 (CAST(2 AS bigint), 2, CAST(2 AS bigint), N'Er der en affaldsspand', 3, 3, 0, 2),
-(CAST(3 AS bigint), 3, CAST(3 AS bigint), N'Antal kvæstede', 4, 3, 0, 3),
-(CAST(4 AS bigint), 3, CAST(4 AS bigint), N'Overfaldsvåben', 5, 3, 0, 3),
-(CAST(5 AS bigint), 3, CAST(5 AS bigint), N'Skal komunaldirektøren underrettes', 2, 3, 0, 5),
+(CAST(3 AS bigint), 3, CAST(3 AS bigint), N'Antal kv├ªstede', 4, 3, 0, 3),
+(CAST(4 AS bigint), 3, CAST(4 AS bigint), N'Overfaldsv├Ñben', 5, 3, 0, 3),
+(CAST(5 AS bigint), 3, CAST(5 AS bigint), N'Skal komunaldirekt├╕ren underrettes', 2, 3, 0, 5),
 (CAST(6 AS bigint), 3, CAST(6 AS bigint), N'Hvilken type kemikalie er i brand?', 5, 3, 0, 5),
 (CAST(7 AS bigint), 3, CAST(7 AS bigint), N'Hvor skal helikopteren lande?', 2, 3, 0, 3);
 IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Column', N'FormId', N'Headline', N'Height', N'Row', N'Static', N'Width') AND [object_id] = OBJECT_ID(N'[FormField]'))
@@ -137,15 +127,9 @@ CREATE INDEX [IX_FormField_FormId] ON [FormField] ([FormId]);
 
 GO
 
-CREATE INDEX [IX_FormFieldValue_CompletedFormId] ON [FormFieldValue] ([CompletedFormId]);
-
-GO
-
-CREATE INDEX [IX_FormFieldValue_FormFieldId] ON [FormFieldValue] ([FormFieldId]);
-
-GO
-
 INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
-VALUES (N'20190516080548_initialModel', N'2.2.4-servicing-10062');
+VALUES (N'20190527081546_initialModel', N'2.2.3-servicing-35854');
 
 GO
+
+
